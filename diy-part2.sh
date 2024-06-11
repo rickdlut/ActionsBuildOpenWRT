6116ca385815f87a7cfbae929b84d66f1507882a
rick <91332065+chenbindlut@users.noreply.github.com>
2024-06-11 18:48:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+!/bin/bash
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
 
 # ==========openclash===========
-# cd package
-# git clone --depth=1 --filter=blob:none --sparse https://github.com/vernesong/OpenClash.git
-# cd OpenClash
-# git sparse-checkout set luci-app-openclash
+ cd package
+ git clone --depth=1 --filter=blob:none --sparse https://github.com/vernesong/OpenClash.git
+ cd OpenClash
+ git sparse-checkout set luci-app-openclash
